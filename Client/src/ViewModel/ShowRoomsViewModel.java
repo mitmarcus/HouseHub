e18e781af70257cba1a52ec4759d629f60cba55f
Nural Hasan <nuralh777@gmail.com>
2023-04-21 10:04:48 +0200

    viewstate

--- Client/src/ViewModel/ShowRoomsViewModel.java
+++ Client/src/ViewModel/ShowRoomsViewModel.java
@@ -1,20 +1,27 @@
 package ViewModel;
 
 import ModelClient.ModelClient;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import ModelClient.Room;
 
 public class ShowRoomsViewModel extends ViewModel
 {
   private ViewState viewState;
   private ModelClient model;
   private ObservableList<String> list;
+  private StringProperty selectedObject;
 
   public ShowRoomsViewModel(ModelClient model,ViewState viewState)
   {
     this.model = model;
     this.viewState = viewState;
     list = FXCollections.observableArrayList();
+    this.selectedObject = new SimpleStringProperty();
     list.add("Room in horsens close to the city center");
     list.add("Room in horsens close to via university collegue");
     list.add("Apartment for 2 next to lovbjerg");
@@ -29,4 +36,15 @@ public class ShowRoomsViewModel extends ViewModel
   {
     return list;
   }
+
+  public void setSelectedObject(String id){
+    this.selectedObject.set(id);
+  }
+
+  public void setId(String id)
+  {
+    viewState.setId(id);
+  }
+
+
 }
--- Client/src/ViewModel/ViewState.java
+++ Client/src/ViewModel/ViewState.java
@@ -2,5 +2,32 @@ package ViewModel;
 
 public class ViewState
 {
+    private String id;
+    private boolean remove;
 
+    public ViewState()
+    {
+        this.id = "";
+        this.remove = false;
+    }
+    public String getId()
+    {
+        return id;
+    }
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+    public void removeId()
+    {
+        this.id = null;
+    }
+    public boolean isRemove()
+    {
+        return  remove;
+    }
+    public void setRemove(boolean remove)
+    {
+        this.remove = remove;
+    }
 }
