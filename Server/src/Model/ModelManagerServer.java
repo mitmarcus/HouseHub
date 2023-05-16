8a45c5b0497157cd24059b37dff7b552c16bc4cb
Nural Hasan <nuralh777@gmail.com>
2023-05-16 11:12:56 +0200

    kjmgcvk

--- Server/src/Model/ModelManagerServer.java
+++ Server/src/Model/ModelManagerServer.java
@@ -18,8 +18,7 @@ import java.rmi.RemoteException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class ModelManagerServer implements ModelServer
-{
+public class ModelManagerServer implements ModelServer {
     private UserDAO userDB;
 
     private ReservationDAO reservationDAO;
@@ -27,16 +26,12 @@ public class ModelManagerServer implements ModelServer
     private RoomDAO roomDB;
 
 
-    public ModelManagerServer()
-    {
-        try
-        {
+    public ModelManagerServer() {
+        try {
             this.reservationDAO = ReservationDAOImpl.getInstance();
             this.userDB = UserDAOImpl.getInstance();
             this.roomDB = RoomDAOImpl.getInstance();
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
@@ -53,24 +48,18 @@ public class ModelManagerServer implements ModelServer
 
     @Override
     public void removeRoom(Room room) {
-        try
-        {
+        try {
             roomDB.getAllRooms();
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             e.getMessage();
         }
     }
 
     @Override
     public Room getRoomByAnnouncement(String announcement) {
-        try
-        {
+        try {
             return roomDB.getRoomByAnnouncement(announcement);
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             e.getMessage();
         }
         return null;
@@ -78,16 +67,14 @@ public class ModelManagerServer implements ModelServer
 
     @Override
     public ArrayList<Room> getAllRooms() {
-        try
-        {
+        try {
             return roomDB.getAllRooms();
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             e.getMessage();
         }
         return null;
     }
+
     @Override
     public void addReservation(Reservation reservation) {
         try {
@@ -121,42 +108,30 @@ public class ModelManagerServer implements ModelServer
 
     @Override
     public void addUser(User user) {
-        try
-        {
+        try {
             userDB.addUser(user);
 
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             e.getMessage();
         }
     }
 
     @Override
     public User getUserByUsername(String username) {
-        try
-        {
+        try {
             return userDB.getUserByUsername(username);
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
     @Override
-    public void removeUserByUsername(String username) {
-        //users.removeUserByUsername(username);
+    public User getUser(String username, String password) {
+        return null;
     }
 
     @Override
-    public void removeUserByPhoneNumber(String phoneNumber) {
-        //users.removeUserByPhoneNumber(phoneNumber);
-    }
-
-
-    @Override public Reservation getReservationById(String id)
-    {
+    public Reservation getReservationById(String id) {
         try {
             return reservationDAO.getReservationById(id);
         } catch (SQLException e) {
@@ -166,22 +141,19 @@ public class ModelManagerServer implements ModelServer
         return null;
     }
 
-    @Override public boolean setRoomReserved(Room room) throws RemoteException
-    {
+    @Override
+    public boolean setRoomReserved(Room room) throws RemoteException {
 
-        try
-        {
+        try {
             return roomDB.setRoomReserved(room);
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             e.getMessage();
         }
         return true;
     }
 
-    @Override public boolean setRoomFree(Room room)
-    {
+    @Override
+    public boolean setRoomFree(Room room) {
         try {
             return roomDB.setRoomFree(room);
         } catch (SQLException e) {
@@ -190,60 +162,33 @@ public class ModelManagerServer implements ModelServer
         return false;
     }
 
-    @Override public void sendFile(String name, byte[] img)
-    {
+    @Override
+    public void sendFile(String name, byte[] img) {
         File folder = new File("Server/src/Images/");
         if (!folder.exists()) {
             folder.mkdirs();
         }
 
-        File outFile= new File(folder.getFreeSpace() + name );
-        try
-        {
-            FileOutputStream fos = new FileOutputStream("Server/src/Images/" + outFile,false);
-            byte [] reciveimg = new byte[img.length];
-            for (int i = 0; i < img.length; i++)
-            {
-                reciveimg[i]=img[i];
+        File outFile = new File(folder.getFreeSpace() + name);
+        try {
+            FileOutputStream fos = new FileOutputStream("Server/src/Images/" + outFile, false);
+            byte[] reciveimg = new byte[img.length];
+            for (int i = 0; i < img.length; i++) {
+                reciveimg[i] = img[i];
             }
             fos.write(reciveimg);
             fos.close();
-        }
-        catch (FileNotFoundException e)
-        {
+        } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
     }
-
-
-    @Override public boolean setUserInfo(User user)
-    {
+    @Override
+    public boolean setUserInfo(User user) {
         //users.setUserInfo(user);
         return true;
 
-    @Override public boolean setUserInfo(User user){
-        try {
-            userDB.setUserInfo(user);
-            return true;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
-
-    public User getUser(String username, String password) {
-        try
-        {
-            return userDB.getUser(username,password);
-        }
-        catch (SQLException e)
-        {
-            e.getMessage();
-        }
-        return null;
-    }
-}}
+}
