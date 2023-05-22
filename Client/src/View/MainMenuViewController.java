2ea038889c006ace702064056f18561f2372db13
Nural Hasan <nuralh777@gmail.com>
2023-05-22 10:45:51 +0200

    dddd

--- Client/src/View/MainMenuViewController.java
+++ Client/src/View/MainMenuViewController.java
@@ -6,6 +6,8 @@ import ViewModel.ShowRoomsViewModel;
 import ViewModel.ViewModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 
@@ -48,4 +50,21 @@ public class MainMenuViewController extends ViewController {
         viewHandler.openView("notifications");
     }
 
+    public void handleLogout(ActionEvent actionEvent) {
+        Alert confirmationDialog = new Alert(Alert.AlertType.CONFIRMATION);
+        confirmationDialog.setTitle("Logout Confirmation");
+        confirmationDialog.setHeaderText("Are you sure you want to log out?");
+        confirmationDialog.setContentText("Click OK to log out or Cancel to stay logged in.");
+
+        // Show the confirmation dialog and wait for the user's response
+        ButtonType response = confirmationDialog.showAndWait().orElse(ButtonType.CANCEL);
+
+        // If the user clicked OK, proceed with the logout
+        if (response == ButtonType.OK) {
+            // Perform the necessary logout operations here
+
+            // Open the main view
+            viewHandler.openView("main");
+        }
+    }
 }
